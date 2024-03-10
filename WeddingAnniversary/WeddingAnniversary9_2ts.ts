b34e213b23dbf77a49cb3ab3671fb82112717612
Kangrok Kim <kimpro@uebermensch.tech>
2024-03-11 00:47:47 +0900

    Update WeddingAnniversary9_2ts.ts

--- WeddingAnniversary/WeddingAnniversary9_2ts.ts
+++ WeddingAnniversary/WeddingAnniversary9_2ts.ts
@@ -10,16 +10,16 @@ function changeColor() {
   
     // loop through the css id list
     for (let i = 0; i < 4; i++) {
-      // generate a random index from 0 to 9
-      let index: number = Math.floor(Math.random() * 10);
-  
-      // get the color from the palette by the index
-      let color: string = palette[index];
-  
-      // assign the color to the element by the css id
-      document.getElementById(cssIdList[i])!.style.color = color;
+        // generate a random index from 0 to 9
+        let index: number = Math.floor(Math.random() * 10);
+    
+        // get the color from the palette by the index
+        let color: string = palette[index];
+    
+        // assign the color to the element by the css id
+        document.getElementById(cssIdList[i])!.style.color = color;
     }
-  }
+}
   
 // call the function every 500 milliseconds
 setInterval(changeColor, 500);
